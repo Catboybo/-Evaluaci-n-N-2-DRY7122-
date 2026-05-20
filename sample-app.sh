ae8edaa0197ec31db68eae6554b35d1a0a6e1223
Catboybo <marinramirez2004@gmail.com>
2026-05-20 19:25:37 +0000

    Fix Jenkins Docker execution

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+docker rm -f samplerunning || true
+docker rmi -f sampleapp || true
+
+rm -rf tempdir
+
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -11,9 +16,7 @@ cp -r static/* tempdir/static/.
 echo "FROM python:3.8-slim" >> tempdir/Dockerfile
 echo "ENV PIP_NO_CACHE_DIR=1" >> tempdir/Dockerfile
 echo "ENV PIP_DISABLE_PIP_VERSION_CHECK=1" >> tempdir/Dockerfile
-echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
 echo "RUN pip install flask --no-cache-dir --progress-bar off" >> tempdir/Dockerfile
-
 
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
@@ -24,6 +27,9 @@ echo "EXPOSE 9999" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
+
 docker build -t sampleapp .
+
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
-docker ps -a 
+
+docker ps -a
