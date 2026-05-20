b0e8c93ebc54b9863b9239d6b80ed294ece96e3c
Catboybo <marinramirez2004@gmail.com>
2026-05-20 18:51:03 +0000

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -8,12 +8,19 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "FROM python:3.8-slim" >> tempdir/Dockerfile
+echo "ENV PIP_NO_CACHE_DIR=1" >> tempdir/Dockerfile
+echo "ENV PIP_DISABLE_PIP_VERSION_CHECK=1" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "RUN pip install flask --no-cache-dir --progress-bar off" >> tempdir/Dockerfile
+
+
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+
 echo "EXPOSE 9999" >> tempdir/Dockerfile
+
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
